164b734b8bec110f4a9a0462ebb211c9de95ec67
Nandhana MJ <nmj18102005@gmail.com>
2026-01-24 12:11:00 +0530

    Ready for deployment

--- backend/model_loader.py
+++ backend/model_loader.py
@@ -1,17 +1,22 @@
+import streamlit as st
+import gdown
 from pathlib import Path
 import tensorflow as tf
 
-# Absolute model path
-MODEL_PATH = (
-    Path(__file__).resolve().parent.parent
-    / "model"
-    / "multilabel_cnn_improved.keras"
-)
+MODEL_PATH = Path("model/multilabel_cnn_improved.keras")
+MODEL_URL = "https://drive.google.com/uc?export=download&id=1WaTwAIm3cENjgL1o_khxyVqWy-jZBPZ8"
 
-_MODEL = None  # singleton
+_MODEL = None
 
 def get_model():
     global _MODEL
     if _MODEL is None:
-        _MODEL = tf.keras.models.load_model(MODEL_PATH)
+        if not MODEL_PATH.exists():
+            with st.spinner("Downloading model (first run only)..."):
+                MODEL_PATH.parent.mkdir(exist_ok=True)
+                gdown.download(MODEL_URL, str(MODEL_PATH), quiet=False)
+
+        with st.spinner("Loading AI model..."):
+            _MODEL = tf.keras.models.load_model(MODEL_PATH)
+
     return _MODEL
